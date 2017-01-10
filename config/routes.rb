56528d760dd5936ac0e3bf339b5ba8e3dff2f7b6
Jessica Genualdi <jessi.genualdi@fcb.com>
2017-01-09 19:44:14 -0600

    rewrote routes and made route index

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/online_resumes' => 'online_resumes#index'
-  post '/online_resumes' => 'online_resumes#create'
-  get '/' => 'pages#index'
-  get '/jess_edits' => 'pages#jess'
-  get '/jess' => 'pages#jess'
- 
+  get '/online_resumes' => 'resumes#index'
 
 end
