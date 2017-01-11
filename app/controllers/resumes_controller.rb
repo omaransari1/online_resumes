9800860ccd5e0fd82efce2425d81cd79ffded10b
Omar Ansari <omaransari1@gmail.com>
2017-01-10 21:08:43 -0600

    Create unirest api request and all student index

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,16 @@
 class ResumesController < ApplicationController
 	def index
+    # @student_capstone = Unirest.get("url/students/#{params[:id]}",
+    #   headers: HEADERS).body
+
+    @students = Unirest.get("https://desolate-island-36210.herokuapp.com").body
+
+    # @student_experience = Unirest.get("url/students/#{params[:id]}",
+    #   headers: HEADERS).body
+
+    # @student_skills = Unirest.get("url/students/#{params[:id]}",
+    #   headers: HEADERS).body
+
     render 'index.html.erb'
 	end
 end
